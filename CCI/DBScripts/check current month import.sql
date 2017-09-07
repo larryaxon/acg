ff760fe7cc9de5a5d5220b8f3d49b064b7417066
larryaxon <larryaxon@gmail.com>
2017-09-07 08:11:23 -0700

    add more data to month check sql

--- CCI/DBScripts/check current month import.sql
+++ CCI/DBScripts/check current month import.sql
@@ -1,11 +1,19 @@
-select * from [dbo].[HostedImportLedger] where billdate = '10/1/2016'
-select * from [dbo].[HostedImportMRCRetail] where billdate = '10/1/2016'
-select * from [dbo].[HostedImportMRCWholesale] where billdate = '10/1/2016'
-select * from [dbo].[HostedImportOCCRetail] where billdate = '10/1/2016'
-select * from [dbo].[HostedImportOCCWholesale] where billdate = '10/1/2016'
-select * from [dbo].[HostedImportTollRetail] where billingyear = 2016 and billingmonth = 10
-select * from [dbo].[HostedImportTollWholesale] where billingyear = 2016 and billingmonth = 10
-select * from [dbo].[HostedImportTaxes] where billdate = '10/1/2016'
+select * from [dbo].[HostedImportLedger] where billdate = '09/1/2017'
+select * from [dbo].[HostedImportMRCRetail] where billdate = '09/1/2017'
+select * from [dbo].[HostedImportMRCWholesale] where billdate = '09/1/2017'
+select * from [dbo].[HostedImportOCCRetail] where billdate = '09/1/2017'
+select * from [dbo].[HostedImportOCCWholesale] where billdate = '09/1/2017'
+select * from [dbo].[HostedImportTollRetail] where billingyear = 2017 and billingmonth = 09
+select * from [dbo].[HostedImportTollWholesale] where billingyear = 2017 and billingmonth = 09
+select * from [dbo].[HostedImportTaxes] where billdate = '09/1/2017'
 
+select *
+  FROM [CityHosted].[dbo].[HostedProcessCycle]
+  where BillingDate = '9/1/2017'
 
+select *
+  FROM [CityHosted].[dbo].[AcctImportsLog]
+  where BillingDate = '9/1/2017'
 
+select * from [dbo].[hostedmatchedmrc] where coalesce([RetailBillDate],[WholesaleBillDate]) = '09/1/2017'
+select * from [dbo].[hostedmatchedocc] where coalesce([RetailBillDate],[WholesaleBillDate]) = '09/1/2017'
